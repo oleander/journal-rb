b9322cb1af5df3a9c38b2dd34f71231b691e2cc0
Linus Oleander <linus@oleander.nu>
2012-01-17 23:32:34 +0100

    Adding current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+  
+  def current_user
+    if uuid = cookies.signed[:uuid]
+      user = User.find_by_uuid(uuid)
+    end
+    
+    if not uuid or not user
+      user = User.create!
+      cookies.signed.permanent[:uuid] = user.uuid
+    end
+    
+    return user
+  end
 end
