8f473e7ee4b32ffbcf9b444e8811ce20563b24a5
Linus Oleander <linus@oleander.nu>
2012-01-19 11:06:17 +0100

    Adding flash message on top of page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require "underscore"
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-//= require "jquery.syntaxhighlighter.min.js"
-//= require "showdown.js"
-//= require "wmd.js"
-//= require "markdown.js"
-//= require "general.js"
+//= require "jquery.syntaxhighlighter.min"
+//= require "showdown"
+//= require "wmd"
+//= require "markdown"
+//= require "general"
--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,4 +1,15 @@
-$(document).ready(function() {
+$(function() {
+  var actions = [
+    "#posts-controller.new-action",
+    "#posts-controller.edit-action"
+  ];
+  
+  if (!_.any($(actions).map(function(index, action) {
+    return !! $(action)[0]
+  }))) {
+    return;
+  }
+  
   $("#highlightCode").click(function(){
     $.SyntaxHighlighter.init({
     'lineNumbers': false,
@@ -6,5 +17,11 @@ $(document).ready(function() {
     });
   });
   
-  new WMD("input", "toolbar", { preview: "preview" });
+  new WMD("input", "toolbar", { preview: "preview" });  
+});
+
+$(function() {
+  $("#note").click(function() {
+    $(this).slideUp("slow");
+  })
 });
