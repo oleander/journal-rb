4bb1fb5aabb2828e8b7d665dc9d82d4a18ca3277
Linus Oleander <linus@oleander.nu>
2012-01-19 01:25:10 +0100

    Loading config file a bit late

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+APP_CONFIG = YAML.load_file "#{Rails.root}/config/config.yml"
+
 # Initialize the rails application
 Journal::Application.initialize!
-
-APP_CONFIG = YAML.load_file "#{Rails.root}/config/config.yml"
