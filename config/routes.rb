862125a5f2af32440b8479bfbfcf729a9135a108
Linus Oleander <linus@oleander.nu>
2012-01-19 01:40:25 +0100

    Root route should not be on top

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-Journal::Application.routes.draw do
-  root to: "projects#index"
-  
+Journal::Application.routes.draw do  
   resources :projects, except: [:show] do
     resources :posts, except: [:show]
   end
+  
+  root to: "projects#index"
   
   match "/auth/:provider/callback" => "authentications#create"
   match "/auth/failure" => "authentications#failure"
