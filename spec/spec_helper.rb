763a1af0735069da421e5ff679a0af049630c063
Linus Oleander <linus@oleander.nu>
2012-01-31 23:22:57 +0100

    Configure spork for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,21 @@
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../../config/environment", __FILE__)
-require "rspec/rails"
-require "rspec/autorun"
+require "rubygems"
+require "spork"
+require "spork/ext/ruby-debug"
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= "test"
+  require File.expand_path("../../config/environment", __FILE__)
+  require "rspec/rails"
+  require "rspec/autorun"
 
-RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+  end
+end
+
+Spork.each_run do
 end
