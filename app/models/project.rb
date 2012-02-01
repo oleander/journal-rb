91c19b69c4bebf812e5cb89070448ee9da743a22
Linus Oleander <linus@oleander.nu>
2012-02-01 01:27:08 +0100

    Validations for Project should be active

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :posts
-  #validates_presence_of :user
-  #validates_uniqueness_of :name
+  validates_presence_of :user
+  validates_uniqueness_of :name
   
   def to_param
     "#{id}-#{name.to_s.downcase.gsub(/[^[:alnum:]]/,'-')}".gsub(/-{2,}/,'-')
