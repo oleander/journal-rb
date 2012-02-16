f26a4a2ee9bf1201e45676182476ea46179bf2a7
Linus Oleander <linus@oleander.nu>
2012-02-16 13:49:16 +0100

    Add public attribute to project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120118204431) do
+ActiveRecord::Schema.define(:version => 20120216122044) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(:version => 20120118204431) do
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "public",     :default => false
   end
 
   add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
